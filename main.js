fc07235ec7e30506eb45a8ea98ed9f6decfc66ae
burxoniddin7212 <liverpul7212@gmail.com>
2022-04-19 09:04:08 +0500

    script

---
+++ main.js
@@ -0,0 +1,43 @@
+var elInput=document.querySelector("#input");
+var elSelect=document.querySelector(".otm");
+var elButton=document.querySelector(".button");
+var elOutput=document.querySelector(".out-massage");
+var grand=110;
+var kantrak=90;
+
+var ins=["O'ZMU", "TATU", "JT", "Vesministr", "Yuridik"];
+
+for(var user of ins){
+  var newOption=document.createElement("option");
+  newOption.textContent=user;
+  newOption.value=user;
+  elSelect.appendChild(newOption);
+}
+
+elButton.addEventListener("click", function(e) {
+    var namball=elInput;
+    
+
+    // if(option.selected.textContent=="O'ZMU" || option.selected.textContent=="O'ZMU"  || option.selected.textContent=="O'ZMU" || option.selected.textContent=="O'ZMU" ||   option.selected.textContent=="O'ZMU"){
+    //   true;
+    // }
+    // else{
+    //   elSelect.style.borderColor="red";
+    //   return
+    // }
+    // elSelect.style.borderColor="black";
+  var ball=elInput.value.trim();
+  //ball=Number(ball);
+  var massage="asdada";
+  if(ball >= grand){
+    massage="Tabriklayman siz o'qishga grand asosida qabul qilindingiz!!!";
+  }
+  else if(ball => kantrak){
+    massage="Siz kantraktga kirdingiz Tabriklayman!!!";
+  }
+  else{
+    massage="Afsuski siz kira olmadingiz Rassiyaga keting";
+  }
+  elOutput.textContent=massage;
+ 
+})
